7ccfc42dcaee27fab60e02af667f7a1c5b391250
thecoolguy2001 <demontewalker12@gmail.com>
2025-06-25 00:01:52 -0400

    fix hope

--- src/components/StripeProvider.jsx
+++ src/components/StripeProvider.jsx
@@ -3,7 +3,7 @@ import { loadStripe } from '@stripe/stripe-js';
 import { Elements } from '@stripe/react-stripe-js';
 
 // TODO: Replace with your actual publishable key
-const stripePromise = loadStripe('pk_test_YOUR_PUBLISHABLE_KEY');
+const stripePromise = loadStripe('pk_test_51RdkNIR6huqF5udsZ8u36LhVTMBUmbaNDel5wEDCYBNtsrTATZWJn5GmnBxTI0aOZJu0v3cCxKBnxASLaDLf7JzG00CMVY7uAQ');
 
 const StripeProvider = ({ children }) => {
   return (
